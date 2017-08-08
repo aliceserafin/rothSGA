7e7951c0b7c77e526f1da8ae472723ba6ea83a35
Matthieu Haudiquet <matthieu.hdqt@gmail.com>
2017-08-07 22:37:36 -0400

    add function summarize_normalized_data

---
+++ R/summarize-normalized-data.R
@@ -0,0 +1,28 @@
+#' Aggregates and summarizes technical replicates on the plate
+#'
+#' Adds mean_size_norm , median_size_norm, and sd_size_norm to a screenmill dataset.
+#'
+#' @param sm_data A screenmill dataset (see [screenmill::read_screenmill])
+#' @param to A column to apply the stats. default is size_plate_norm (see[rothSGA::apply_plate_normalization]) (as a string).
+#'
+#' @md
+#' @export
+
+
+summarize_normalized_data <- function(sm_data, to = size_plate_norm){
+
+  assertthat::assert_that(assertthat::is.string(to))
+
+  sm_data$to_tmp <- sm_data[[to]]
+
+  sm_data %>%
+  group_by(plate_id, strain_name) %>%
+  mutate(
+         n                = n(to),
+         mean_size_norm   = mean(to),
+         median_size_norm = median(to),
+         sd_size_norm     = sd(to)
+  ) %>%
+    ungroup()
+
+}
