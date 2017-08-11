f1f63c39d34a04dbb162a9828fcedc68952ae5d2
Matthieu Haudiquet <matthieu.hdqt@gmail.com>
2017-08-11 19:10:29 -0400

    Fix add_biological_replicates

--- R/add-biological-replicates.R
+++ R/add-biological-replicates.R
@@ -5,17 +5,17 @@
 #' for every plate. Currently only supports single timepoint analysis.
 #'
 #' @param dir Path to screenmill analysis directory
+#' @param file Name of resulting CSV file.
 #' @param overwrite A flag indicating whether to overwrite an existing biological replicate
 #' annotation file.
 #'
 #' @import dplyr
 #' @import assertthat
-#' @importFrom readr write_csv
+#' @importFrom readr write_csv read_csv
 #' @md
 #' @export
 
-add_biological_replicates <- function(dir, overwrite = FALSE) {
-
+add_biological_replicates <- function(dir, file, overwrite = FALSE) {
   anno_path <- file.path(dir, 'screenmill-annotations.csv')
   quer_path <- file.path(dir, 'screenmill-queries.csv')
 
@@ -23,18 +23,19 @@ add_biological_replicates <- function(dir, overwrite = FALSE) {
     dir.exists(dir),
     file.exists(anno_path),
     file.exists(quer_path),
-    assertthat::is.flag(overwrite)
+    assertthat::is.flag(overwrite),
+    assertthat::is.string(file)
   )
 
   goal <- file.path(dir, 'biological-replicate-annotation.csv')
 
   # Biological replicates need to be manually entered into the following table
   if (!file.exists(goal) || overwrite) {
-    anno    <- read_csv(anno_path)
-    queries <- read_csv(quer_path)
+    anno    <- readr::read_csv(anno_path, col_types = cols_only(query_id = 'c', plate_id = 'c', group = 'i', position = 'i'))
+    queries <- readr::read_csv(quer_path, col_types = cols_only(query_id = 'c', query_name = 'c'))
     left_join(anno, queries) %>%
       mutate(bio_replicate = '') %>%
-      select(plate_id, query_name, query_id, group, position) %>%
+      select(plate_id, query_name, query_id, group, position, bio_replicate) %>%
       write_csv(goal)
     message('Please manually annotate biological replicates in:\n  ', goal)
   } else {
