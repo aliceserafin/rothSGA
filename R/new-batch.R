89a9810c51c596e9af23a214e621aed737228ed7
Matthieu Haudiquet <matthieu.hdqt@gmail.com>
2017-08-16 15:56:44 -0400

    Changes to batch analysis pipeline

--- R/new-batch.R
+++ R/new-batch.R
@@ -12,12 +12,25 @@
 #' @importFrom stringr str_replace_all fixed
 #' @export
 
-new_batch_processing <- function(proj_dir = '.', data_dir = 'data', author = getOption('devtools.name')) {
+new_batch <- function(proj_dir = '.', data_dir = 'data', author = getOption('devtools.name')) {
   assertthat::assert_that(dir.exists(proj_dir), dir.exists(data_dir), assertthat::is.string(author %||% ''))
+  new_batch_processing(proj_dir, data_dir, author)
+  new_quality_control(proj_dir, data_dir, author)
+}
 
-  read_lines(system.file('templates/batch-analysis-pipeline.Rmd', package = 'rothSGA')) %>%
+
+new_batch_processing <- function(proj_dir, data_dir, author) {
+  read_lines(system.file('templates/batch-processing.Rmd', package = 'rothSGA')) %>%
     str_replace_all(fixed('{{{ author }}}'),   author %||% '') %>%
     str_replace_all(fixed('{{{ data_dir }}}'), data_dir) %>%
     str_replace_all(fixed('{{{ date }}}'),     Sys.Date()) %>%
     write_lines(file.path(proj_dir, 'Batch-processing.Rmd'))
+}
+
+new_quality_report <- function(proj_dir, data_dir, author) {
+  read_lines(system.file('templates/quality-control.Rmd', package = 'rothSGA')) %>%
+    str_replace_all(fixed('{{{ author }}}'),   author %||% '') %>%
+    str_replace_all(fixed('{{{ data_dir }}}'), data_dir) %>%
+    str_replace_all(fixed('{{{ date }}}'),     Sys.Date()) %>%
+    write_lines(file.path(proj_dir, 'quality-control.Rmd'))
 }
