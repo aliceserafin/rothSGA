d43efc7929916918c8931dd2e85c2e5707fe8a7f
Matthieu Haudiquet <matthieu.hdqt@gmail.com>
2017-08-20 18:31:38 -0400

    improved efficiency

--- R/leave-one-out-mean.R
+++ R/leave-one-out-mean.R
@@ -1,4 +1,4 @@
-#' Generic function that drops abberant values of small distribution
+#' Generic function that drops abberant values of small distribution (2<n<8) and returns a new median if it significantly reduces the sd
 #'
 #' @param x distribution (numeric vector)
 #'
@@ -11,14 +11,15 @@ leave_one_out_mean <- function(x){
 
   xx <- data.frame(dist = x)
   xx <- data.frame(dist = drop_na(xx))
+  sd <- sd(xx$dist)
 
-  if (sd(xx$dist) > 0.15*mean(xx$dist) & length(xx$dist) > 2) {
+  if (sd > 0.15*mean(xx$dist) & length(xx$dist) > 2 & length(xx$dist < 8)) {
 
-    dropped_min <- xx %>% filter(!dist == min(dist)) %>% summarize(mean = mean(dist), sd = sd(dist))
-    dropped_max <- xx %>% filter(!dist == max(dist)) %>% summarize(mean = mean(dist), sd = sd(dist))
+    dropped_min <- xx %>% filter(!dist == min(dist)) %>% summarize(mean = mean(dist), sd = sd(dist)) %>% drop_na()
+    dropped_max <- xx %>% filter(!dist == max(dist)) %>% summarize(mean = mean(dist), sd = sd(dist)) %>% drop_na()
     y <- rbind(dropped_min, dropped_max) %>% filter(sd == min(sd))
 
-    if (min(c(dropped_max$sd,dropped_min$sd)) < 0.9*sd(x)) {
+    if (min(c(dropped_max$sd,dropped_min$sd)) < 0.9*sd) {
       return(y$mean)
     } else {
       return(mean(xx$dist))
@@ -31,13 +32,13 @@ leave_one_out_mean <- function(x){
 
 
 
-# TEST
-# library(tidyverse)
-# x = distribution$dist
-# distribution <- data.frame(dist = c(1, 2, NA, NA, NA, NA))
-# mean(distribution$dist)
-# sd(distribution$dist)
-# sd(distribution$dist) / mean(distribution$dist)
-#
-# leave_one_out_mean(distribution$dist)
+
+library(tidyverse)
+#x = distribution$dist
+distribution <- data.frame(dist = c(1, NA))
+mean(distribution$dist)
+sd(distribution$dist)
+sd(distribution$dist) / mean(distribution$dist)
+
+leave_one_out_mean(distribution$dist)
 
